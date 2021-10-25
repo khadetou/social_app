d829b1dd5201bfe9274d9af38344f49c3b58c174
khadetou <khadetou96@gmail.com>
2021-10-25 21:51:05 +0000

    sign in screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:socialapp/pages/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       title: "Social network app",
       debugShowCheckedModeBanner: false,
-      home: Text("Hello world"),
+      home: const Home(),
     );
   }
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:socialapp/data/data.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -8,8 +9,56 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool _isAuth = false;
+
+  Widget buildAuthScreen() {
+    return const Text("Authenticated");
+  }
+
+  Widget buildUnAuthScreen() {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [Colors.teal, Colors.purple],
+          ),
+        ),
+        alignment: Alignment.center,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              "FlutterShare",
+              style: TextStyle(
+                fontFamily: "Signatra",
+                fontSize: 90,
+                color: Colors.white,
+              ),
+            ),
+            GestureDetector(
+              onTap: () => print("sign in tapped"),
+              child: Container(
+                width: 260.0,
+                height: 60.0,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(Images.googleSignIn),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Text('Home');
+    return _isAuth ? buildAuthScreen() : buildUnAuthScreen();
   }
 }

1e14cfde1ddfb922a8cecdfd3abb48738363b445
khadetou <khadetou96@gmail.com>
2021-10-27 16:08:23 +0000

    save auth users in users

--- lib/pages/create_account.dart
+++ lib/pages/create_account.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:socialapp/widgets/header.dart';
 
 class CreateAccount extends StatefulWidget {
   const CreateAccount({Key? key}) : super(key: key);
@@ -8,8 +9,70 @@ class CreateAccount extends StatefulWidget {
 }
 
 class _CreateAccountState extends State<CreateAccount> {
+  final _formKey = GlobalKey<FormState>();
+  String? username;
+
+  submit() {
+    _formKey.currentState!.save();
+    Navigator.pop(context, username);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Text("Create Account");
+    return Scaffold(
+      appBar: header(
+        titleText: "Set up your profile",
+      ),
+      body: Column(
+        children: <Widget>[
+          const Padding(
+            padding: EdgeInsets.only(top: 25),
+            child: Center(
+              child: Text(
+                "Create a username",
+                style: TextStyle(fontSize: 25.0),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+              key: _formKey,
+              child: TextFormField(
+                onSaved: (val) => username = val,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Username",
+                    labelStyle: TextStyle(
+                      fontSize: 15.0,
+                    ),
+                    hintText: "Must be at least 3 characters"),
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: submit,
+            child: Container(
+              width: 200.0,
+              height: 35.0,
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(7.0),
+              ),
+              child: const Center(
+                child: Text(
+                  "Submit",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,14 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:socialapp/data/data.dart';
 import 'package:socialapp/pages/activity_feed.dart';
+import 'package:socialapp/pages/create_account.dart';
 import 'package:socialapp/pages/profile.dart';
 import 'package:socialapp/pages/search.dart';
 import 'package:socialapp/pages/timeline.dart';
 import 'package:socialapp/pages/upload.dart';
 
 final GoogleSignIn googleSignIn = GoogleSignIn();
+final userRef = FirebaseFirestore.instance.collection("users");
+final DateTime timestamp = DateTime.now();
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -55,12 +59,40 @@ class _HomeState extends State<Home> {
 
   handleSignIn(GoogleSignInAccount? account) {
     if (account != null) {
+      createUser();
       setState(() {
         _isAuth = true;
       });
     } else {
       setState(() {
         _isAuth = false;
+      });
+    }
+  }
+
+  //Create user in firestore
+  createUser() async {
+    //1°) Check if user exists in users collextion in database according to their id.
+    final GoogleSignInAccount? user = googleSignIn.currentUser;
+    final DocumentSnapshot doc = await userRef.doc(user!.id).get();
+    if (!doc.exists) {
+      // 2°) If the user doesn't exist, then we want to take them to the create account page.
+      final username = await Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const CreateAccount(),
+        ),
+      );
+      //3°)get username from create account, use it to make new document in users collection.
+
+      userRef.doc(user.id).set({
+        "id": user.id,
+        "username": username,
+        "photoUrl": user.photoUrl,
+        "email": user.email,
+        "displayName": user.displayName,
+        "bio": "",
+        "timestamp": timestamp
       });
     }
   }
@@ -90,12 +122,13 @@ class _HomeState extends State<Home> {
   Widget buildAuthScreen() {
     return Scaffold(
       body: PageView(
-        children: const <Widget>[
-          TimeLine(),
-          ActivityFeed(),
-          Upload(),
-          Search(),
-          Profile()
+        children: <Widget>[
+          // TimeLine(),
+          ElevatedButton(onPressed: logout, child: const Text("Log out")),
+          const ActivityFeed(),
+          const Upload(),
+          const Search(),
+          const Profile()
         ],
         controller: pageController,
         onPageChanged: onPageChanged,

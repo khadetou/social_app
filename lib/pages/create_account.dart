0547a4f534e751d71b251e4085d89ee5228e3305
khadetou <khadetou96@gmail.com>
2021-10-27 17:54:28 +0000

    validating create account form

--- lib/pages/create_account.dart
+++ lib/pages/create_account.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:socialapp/widgets/header.dart';
 
@@ -9,12 +11,21 @@ class CreateAccount extends StatefulWidget {
 }
 
 class _CreateAccountState extends State<CreateAccount> {
+  // final _scafoldKey = GlobalKey<ScaffoldState>();
   final _formKey = GlobalKey<FormState>();
   String? username;
 
   submit() {
-    _formKey.currentState!.save();
-    Navigator.pop(context, username);
+    final form = _formKey.currentState;
+
+    if (form!.validate()) {
+      form.save();
+      SnackBar snackBar = SnackBar(content: Text("Welcome $username!"));
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      Timer(const Duration(seconds: 2), () {
+        Navigator.pop(context, username);
+      });
+    }
   }
 
   @override
@@ -38,7 +49,17 @@ class _CreateAccountState extends State<CreateAccount> {
             padding: const EdgeInsets.all(16.0),
             child: Form(
               key: _formKey,
+              autovalidateMode: AutovalidateMode.onUserInteraction,
               child: TextFormField(
+                validator: (val) {
+                  if (val!.trim().length < 3 || val.isEmpty) {
+                    return "Username too short";
+                  } else if (val.trim().length > 12) {
+                    return "Username too long";
+                  } else {
+                    return null;
+                  }
+                },
                 onSaved: (val) => username = val,
                 decoration: const InputDecoration(
                     border: OutlineInputBorder(),

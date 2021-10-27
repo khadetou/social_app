3ecaf033db002853578a58b711a3b584447fab4d
khadetou <khadetou96@gmail.com>
2021-10-27 14:06:47 +0000

    crud

--- lib/pages/timeline.dart
+++ lib/pages/timeline.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:socialapp/widgets/header.dart';
 import 'package:socialapp/widgets/progress.dart';
@@ -15,64 +14,11 @@ class TimeLine extends StatefulWidget {
 
 class _TimeLineState extends State<TimeLine> {
   List<dynamic> users = [];
-  var firestoreDb = FirebaseFirestore.instance.collection("users").snapshots();
+
   @override
   void initState() {
-    // getUsers();
-    // getUserById();
     super.initState();
   }
-
-//BEST APPROCH USING THE ASYNC AWAIT
-
-//GET ALL USERS
-  // getUsers() async {
-  //compound queries
-  //   final QuerySnapshot snapshot = await userRef
-  //       .where("username", isEqualTo: "khadetou")
-  //       .where("postsCount", isLessThan: 3)
-  //       .get();
-  //   for (var doc in snapshot.docs) {
-  //     print(doc.data());
-  //   }
-  // }
-
-//GET USER BY ID
-
-  // getUserById() async {
-  //   const String id = "Lg3xPylFz9J0rz5xAS97";
-
-  //   final DocumentSnapshot doc = await userRef.doc(id).get();
-  //   print(doc.data());
-  //   print(doc.exists);
-  // }
-
-  /// ---------------------------------------- */
-//Then catch method
-
-  // getUserById() {
-  //   const String id = "Lg3xPylFz9J0rz5xAS97";
-
-  //   userRef.doc(id).get().then(
-  //     (DocumentSnapshot doc) {
-  //       print(doc.data());
-  //       print(doc.exists);
-  //       print(doc.id);
-  //     },
-  //   );
-  // }
-
-  // getUsers() {
-  //   userRef.get().then(
-  //     (QuerySnapshot snapshot) {
-  //       for (var doc in snapshot.docs) {
-  //         print(doc.data());
-  //         print(doc.exists);
-  //         print(doc.id);
-  //       }
-  //     },
-  //   );
-  // }
 
   @override
   Widget build(BuildContext context) {
